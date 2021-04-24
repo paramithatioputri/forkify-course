de296971ee404ae0c00ee8d585b15817c9fdbbfc
Paramitha Aryanand Tioputri <paramithaaryananda31@gmail.com>
2021-04-24 17:45:22 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const TIMEOUT_SEC = 10;
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const RES_PER_PAGE = 10;
+export const KEY = '9210038a-bd1e-47e1-9fba-cbdee7d92512';
+export const MODAL_CLOSE_SEC = 2.5;
